269f05d9d18096b7f10564ce08c87d08c9f9966e
Sergey Sapanus <s.sapanus@systteh.ru>
2020-01-24 18:31:53 +0200

    feat: add run.ps1 & vscode python profile

---
+++ run.ps1
@@ -0,0 +1,9 @@
+﻿$pythonPath = "c:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64"
+
+$env:Path += ";$pythonPath"
+$env:PathExt += ";.py"
+
+$scriptsRoot = "C:\Users\admin\source\repos\SergeySapanus\IntroPython"
+$scriptsQuickSort = "$scriptsRoot\PythonConcoleApp\PythonConcoleApp\04-QuickSort"
+
+python "$scriptsQuickSort\01_loop_sum.py" #>> output.txt
